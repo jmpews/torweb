828c5dd7749f9c641134232ee738f7ce3ed94535
jmpews <jmpews@gmail.com>
2016-03-10 15:35:58 +0800

    add config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+
+PORT = 9000
+DEBUG = True
+APP_NAME = 'sxuhelp'
+TITLE = 'sxuhelp'
+TEMPLATE = 'tornado'  # jinja2/mako/tornado
+
+Enable_Url_Count=True
+
+# BACKEND_MYSQL="mysql+pymysql://root:root@192.168.33.10/sxuhelp"
+# BACKEND_MONGO= "mongodb://192.168.33.10/sxuhelp"
+
+BACKEND_MYSQL="mysql+pymysql://root:root@127.0.0.1/sxuhelp"
+BACKEND_MONGO= "mongodb://127.0.0.1/sxuhelp"
+BACKEND_REDIS=('localhost',6379,0)
+
+# BACKEND_MYSQL="mysql+pymysql://root:root@10.0.250.61/sxuhelp"
+# BACKEND_MONGO= "mongodb://10.0.250.61/sxuhelp"
+
+COOKIE_SECRET = "6aOO5ZC55LiN5pWj6ZW/5oGo77yM6Iqx5p+T5LiN6YCP5Lmh5oSB44CC"
+
+domain='weixin2.linevery.com'
+

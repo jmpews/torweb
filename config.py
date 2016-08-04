18e00f060e56c693e52a16d328c2708046d085fc
jmpews <jmpews@gmail.com>
2016-08-04 15:52:43 +0800

    假如测试测试数据库

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ BACKEND_MYSQL = {
     'max_connections': 20,
     'stale_timeout': 300,
     'user': 'root',
-    'password': 'root',
+    'password': 'qwaszx',
     'host': mysql_host if mysql_host else '127.0.0.1',
-    'port': mysql_port if mysql_port else '3306'
+    'port': int(mysql_port) if mysql_port else 3306
 }
 BACKEND_MONGO = "mongodb://127.0.0.1/torweb"
 BACKEND_REDIS = ('localhost', 6379, 0)

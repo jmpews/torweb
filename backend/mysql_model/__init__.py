cb41c73670d29e5aab5930e4abd8a3f0fb0053a2
jmpews <jmpews@gmail.com>
2016-08-04 14:17:00 +0800

    通过环境变量取mysq连接信息(docker)

--- backend/mysql_model/__init__.py
+++ backend/mysql_model/__init__.py
@@ -2,14 +2,18 @@ from config import BACKEND_MYSQL
 from peewee import Model
 from playhouse.pool import PooledMySQLDatabase
 
-#PooledMySQLDatabase
+# PooledMySQLDatabase
 db_mysql = PooledMySQLDatabase(
     BACKEND_MYSQL['database'],
     max_connections=BACKEND_MYSQL['max_connections'],
     stale_timeout=BACKEND_MYSQL['stale_timeout'],  # 5 minutes.
     user=BACKEND_MYSQL['user'],
-    password=BACKEND_MYSQL['password']
+    password=BACKEND_MYSQL['password'],
+    host=BACKEND_MYSQL['host'],
+    port=BACKEND_MYSQL['port']
 )
+
+
 class BaseModel(Model):
     class Meta:
         database = db_mysql
--- config.py
+++ config.py
@@ -1,21 +1,27 @@
+import os
+
 PORT = 9000
 DEBUG = True
-
 # peewee
 # BACKEND_MYSQL="mysql+pool://root:root@127.0.0.1/torweb?max_connections=20&stale_timeout=300"
-BACKEND_MYSQL={
-    'database':'torweb',
-    'max_connections':20,
-    'stale_timeout':300,
-    'user':'root',
-    'password':'root',
+mysql_port = os.getenv('DB_PORT_3306_TCP_PORT')
+mysql_host = os.getenv('DB_PORT_3306_TCP_ADDR')
+
+BACKEND_MYSQL = {
+    'database': 'torweb',
+    'max_connections': 20,
+    'stale_timeout': 300,
+    'user': 'root',
+    'password': 'root',
+    'host': mysql_host if mysql_host else '127.0.0.1',
+    'port': mysql_port if mysql_port else '3306'
 }
-BACKEND_MONGO= "mongodb://127.0.0.1/torweb"
-BACKEND_REDIS=('localhost',6379,0)
+BACKEND_MONGO = "mongodb://127.0.0.1/torweb"
+BACKEND_REDIS = ('localhost', 6379, 0)
 
 COOKIE_SECRET = "6aOO5ZC55LiN5pWj6ZW/5oGo77yM6Iqx5p+T5LiN6YCP5Lmh5oSB44CC"
 
-log_path='./'
+log_path = './'
 
-domain='weixin.linevery.com'
+domain = 'weixin.linevery.com'
 

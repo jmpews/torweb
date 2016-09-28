16bc97820ff9bcc1dc2b0ccfa72ebc0937fcbc2a
jmpews <jmpews@gmail.com>
2016-09-28 23:14:42 +0800

    添加一个脚本来导入数据python tests/test_mysql.py

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -1,87 +1,130 @@
 import sys, os
-
+import os.path
 sys.path.append(os.path.dirname(sys.path[0]))
-from db.mysql_model import db_mysql
-from db.mysql_model.user import User
-from db.mysql_model.post import Post, PostReply
 
-if User.table_exists():
-    print('[User]表已经存在')
-else:
-    db_mysql.create_table(User)
+from settings.config import config
+from peewee import Model, MySQLDatabase
+from custor.logger import logger
 
-if Post.table_exists():
-    print('[Post]表已经存在')
-else:
-    db_mysql.create_table(Post)
-
-if PostReply.table_exists():
-    print('[PostReply]表已经存在')
-else:
-    db_mysql.create_table(PostReply)
-
-PostReply.delete().execute()
-Post.delete().execute()
-User.delete().execute()
+mysqldb = MySQLDatabase('',
+                        user=config.BACKEND_MYSQL['user'],
+                        password=config.BACKEND_MYSQL['password'],
+                        host=config.BACKEND_MYSQL['host'],
+                        port=config.BACKEND_MYSQL['port'])
 
 
-def test_user():
-    user = User.get_by_username('admin')
-    if user:
-        user.delete_instance()
-        print('already exist <admin>,so delete it!')
-    else:
-        print('<admin> not exist')
-    user = User.new(username='admin', nickname='admin', password='admin')
-    print('new user <admin:admin>', user)
+def create_test_data(db_mysql):
+    from db.mysql_model.user import User, Profile, Follower, ChatLog
+    from db.mysql_model.post import Post, PostReply, PostCategory, PostTopic, CollectPost
+    from db.mysql_model.common import Notification
+    from db.mysql_model.blog import BlogPost, BlogPostLabel, BlogPostCategory
 
-    user.set_password('root')
-    print('change <admin> password to <root>')
+    logger.debug("DataBase is not exist, so create test data.")
 
-    if User.auth('admin', 'admin'):
-        print('auth success!')
-    else:
-        print('auth failed!')
+    # -------------------- 建表 ---------------
+    db_mysql.create_tables([User, ChatLog, PostCategory, PostTopic, Post, PostReply, CollectPost, Profile, Follower, Notification, BlogPostCategory, BlogPost, BlogPostLabel], safe=True)
+    user_admin = User.new(username='admin', email='admin@jmp.com', password='admin')
+    user_test = User.new(username='test', email='test@jmp.com', password='test')
 
-    if User.exist('admin'):
-        print('admin exist')
-    else:
-        print('admin not exist')
-    print('User count ', User.count())
+    # -------------------- 测试关注功能 ---------------
+    Follower.create(user=user_admin, follower=user_test)
+
+    # -------------------- 测试分类功能 --------------
+    logger.debug('''
+    版块分类
+    专业:
+        计算机
+    学习:
+        学习资料、考研资料、家教、竞赛
+
+    生活:
+        共享账号、电影资源、常用软件、电脑故障
+
+    爱好:
+        摄影、健身
+
+    未分类:
+        校园通知、讨论
+    ''')
+
+    postcategory0 = PostCategory.create(name='学习', str='study')
+    postcategory1 = PostCategory.create(name='专业', str='major')
+    postcategory2 = PostCategory.create(name='生活', str='live')
+    postcategory3 = PostCategory.create(name='爱好', str='hobby')
+
+    posttopic0 = PostTopic.create(category=postcategory0, name='学习资料', str='study-material')
+    posttopic1 = PostTopic.create(category=postcategory0, name='考研资料', str='study-advance-material')
+    posttopic2 = PostTopic.create(category=postcategory0, name='竞赛', str='study-competition')
+    posttopic3 = PostTopic.create(category=postcategory0, name='请教', str='study-advice')
+
+    posttopic4 = PostTopic.create(category=postcategory1, name='计算机', str='major-computer')
+
+    posttopic5 = PostTopic.create(category=postcategory2, name='电影资源', str='live-movie')
+    posttopic6 = PostTopic.create(category=postcategory2, name='共享账号', str='live-account')
+    posttopic7 = PostTopic.create(category=postcategory2, name='电脑故障', str='live-computer-repair')
+
+    posttopic8 = PostTopic.create(category=postcategory3, name='摄影', str='hobby-photography')
+    posttopic9 = PostTopic.create(category=postcategory3, name='健身', str='hobby-fitness')
+
+    posttopic10 = PostTopic.create(name='通知', str='notice')
+    posttopic11 = PostTopic.create(name='讨论', str='discussion')
 
 
-test_user()
-
-
-def test_post():
-    user = User.get_by_username('admin')
+    # ---------------- 测试新文章 --------------
     post = Post.create(
-            title="test_vul_scan",
-            content="vul_scan_content_04.27_12:47",
-            user=user
+        topic=posttopic0,
+        title='test',
+        content=tmp_post,
+        user=user_admin
     )
-    post.up_visit()
-    post.up_collect()
-    print('new post <test_vul_scan:admin>')
+
+    # ---------------- 测试通知 --------------
+    Notification.new_post(post)
+
+    # ------------测试新回复--------------
     postreply = PostReply.create(
             post=post,
-            user=user,
-            content='test_reply'
+            user=user_test,
+            content='test'
     )
-    print('new postreply <test_reply:admin>')
-    postreply.up_like()
-    post.up_reply()
-
-    post_test = Post.create(
-            title="IDS是一种积极主动的安全防护技术",
-            content="入侵检测系统的核心价值在于通过对全网信息的分析，了解信息系统的安全状况，进而指导信息系统安全建设目标以及安全策略的确立和调整，而入侵防御系统的核心价值在于安全策略的实施—对黑客行为的阻击",
-            user=user
-    )
-    postreply_test = PostReply.create(
-            post=post,
-            user=user,
-            content='入侵检测系统需要部署在网络内部，监控范围可以覆盖整个子网，包括来自外部的数据以及内部终端之间传输的数据，入侵防御系统则必须部署在网络边界，抵御来自外部的入侵，对内部攻击行为无能为力。'
-    )
+    post.update_latest_reply(postreply)
 
 
-test_post()
+    # ---------------- 测试Blog --------------
+    bpc0 = BlogPostCategory.create(name='Tornado', str='Tornado')
+    bp0 = BlogPost.create(title='Tornado', category=bpc0, content='Tornado content')
+    BlogPostLabel.add_post_label('python,tornado', bp0)
+
+    # ---------------- 测试chat --------------
+    chat_log_0 = ChatLog.create(me=user_admin, other=user_test, content='self>other')
+    chat_log_0 = ChatLog.create(me=user_test, other=user_admin, content='other>self')
+
+
+
+def mysql_db_init(db_mysql, db_name):
+    if not mysqldb.execute_sql("select * from information_schema.schemata where schema_name = '{0}';".format(config.BACKEND_MYSQL['database'])).fetchone():
+        mysqldb.execute_sql("create database {0} default character set utf8 default collate utf8_general_ci;".format(config.BACKEND_MYSQL['database']))
+    else:
+        pass
+        mysqldb.execute_sql("drop database torweb")
+        mysqldb.execute_sql("create database {0} default character set utf8 default collate utf8_general_ci;".format(config.BACKEND_MYSQL['database']))
+
+    # create_test_data(db_mysql)
+    logger.debug('load db from {0}.'.format(db_name))
+    import os
+    os.system('mysql -u{0} -p{1} {2} <'.format(config.BACKEND_MYSQL['user'], config.BACKEND_MYSQL['password'], config.BACKEND_MYSQL['database'])+os.getcwd()+db_name)
+    mysqldb.close()
+
+import html
+tmp_post = '''
+<pre><code>
+'''+html.escape('''
+test.test
+''')+ '''
+</code></pre>
+'''
+
+if __name__ == '__main__':
+    from db.mysql_model import db_mysql
+    db_name = '/db/torweb.sql'
+    mysql_db_init(db_mysql, db_name)

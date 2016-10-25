ae934b7c852917a9bc3f1aebd800c46b7c3b6c19
jmpews <jmpews@gmail.com>
2016-10-25 21:39:40 +0800

    update css

--- tests/test_thread_future.py
+++ tests/test_thread_future.py
@@ -7,7 +7,7 @@ from tornado import gen
 sys.path.append(os.path.dirname(sys.path[0]))
 
 import time
-from custor.handlers.basehandler import run_with_thread_future
+from custor.decorators import run_with_thread_future
 
 @run_with_thread_future(None)
 def thread_sleep(self, args):

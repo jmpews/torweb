4405df7f9509a48daf24beeabd45ee39d3db4519
jmpews <jmpews@gmail.com>
2016-10-26 17:24:54 +0800

    fix bug

--- app/post/utils.py
+++ app/post/utils.py
@@ -4,8 +4,14 @@ from db.mysql_model.post import CollectPost
 def get_post_user_ext(post, user):
     ext = type('ext', (), {})
     is_collect = CollectPost.is_collect(post, user),
-    is_own = post.check_own(user)
-    is_auth = user.is_admin()
+
+    if user:
+        is_own = post.check_own(user)
+        is_auth = user.is_admin()
+    else:
+        is_own = False
+        is_auth = False
+
     setattr(ext, 'is_collect', is_collect)
     setattr(ext, 'is_own', is_own)
     setattr(ext, 'is_auth', is_auth)
